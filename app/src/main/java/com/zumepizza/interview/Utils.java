699c069071d2032357b3d11a6d30ae34f94d7149
mikeescom <mike_matematicas@ciencias.unam.mx>
2018-04-13 20:33:16 -0700

    Libraries

---
+++ app/src/main/java/com/zumepizza/interview/Utils.java
@@ -0,0 +1,34 @@
+package com.zumepizza.interview;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.zumepizza.interview.data.Topping;
+
+import java.util.List;
+
+/**
+ * Created by miguelsaavedralozano on 4/13/18.
+ */
+
+public class Utils {
+    public static <T> T decodeResp(Class<T> classOfT, String encoded) {
+        T retVal = null;
+        try {
+            Gson gson = new Gson();
+            retVal = gson.fromJson(encoded, classOfT);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return retVal;
+    }
+
+    public static String getToppings (List<Topping> toppings) {
+        String toppingString = "";
+
+        for (Topping topping: toppings) {
+            toppingString = toppingString.concat(topping.getName()).concat(", ");
+        }
+
+        return  toppingString.substring(0, toppingString.length()-2);
+    }
+}
